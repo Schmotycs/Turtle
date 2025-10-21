6ad46f38186d9d13cb5b2e31f29f8effc2e30dfc
Devin Kahe <DEK@ivu.de>
2025-10-21 13:34:49 +0200

    Erster Commit

---
+++ CSV_Aufteilen.py
@@ -0,0 +1,95 @@
+import csv
+from pathlib import Path
+import numpy
+
+
+
+Pfad = Path("C:/Users/dek/Documents/Turtle/E20-44521.xml-251002-110553-opt_1-tracks.csv")
+
+# Datei lesen
+with open(Pfad, mode="r", encoding="utf-8", newline="") as datei:
+    daten = datei.readlines()
+
+# Zeilen bereinigen
+zeilen = []
+for i in range(len(daten)):
+    saubere_zeile = daten[i].strip()
+    zeilen.append(saubere_zeile) 
+
+tabellen = []
+aktuelle_überschrift = None
+aktuelle_tabelle = []
+
+leere_zeile_index = 0
+in_tabelle = False
+
+for i in range(len(zeilen)):
+    zeile = zeilen[i]
+
+    if zeile == "":
+        leere_zeile_index += 1
+        continue
+
+    if ";" not in zeile:
+        if aktuelle_tabelle != []:
+            tabellen.append((aktuelle_überschrift, aktuelle_tabelle))
+            aktuelle_tabelle = []
+        aktuelle_überschrift = zeile
+        leere_zeile_index = 0
+        in_tabelle = False
+
+    elif leere_zeile_index >= 2:
+        aktuelle_tabelle = []
+        in_tabelle = True
+        leere_zeile_index = 0
+        aktuelle_tabelle.append(zeile.split(";"))
+
+    elif in_tabelle:
+        aktuelle_tabelle.append(zeile.split(";"))
+
+# Letzte Tabelle speichern
+if aktuelle_tabelle != []:
+   tabellen.append((aktuelle_überschrift, aktuelle_tabelle))
+
+# Ausgabeordner
+Ausgabe = Path("C:/Users/dek/Documents/Turtle/Tabellen")
+Ausgabe.mkdir(exist_ok=True)
+überschriften = []
+
+# Tabellen speichern
+for i in range(len(tabellen)):
+    überschrift = tabellen[i][0]
+    tabelle = tabellen[i][1]
+
+    # Dateiname bereinigen
+    dateiname_basis = f"Tabelle_{i+1}"
+    dateiname = f"{dateiname_basis}.csv"
+
+    überschriften.append(dateiname)
+    dateipfad = Ausgabe / dateiname
+    print(f"dateipfad {dateipfad}")
+
+    with open(dateipfad, mode="w", newline="", encoding="utf-8") as ausgabedatei:
+        writer = csv.writer(ausgabedatei, delimiter=";")
+        for k in range(len(tabelle)):
+            writer.writerow(tabelle[k])
+
+    print(f"Tabelle '{überschrift}' gespeichert als '{dateiname}'")
+
+
+dateipfad2 = Path("C:/Users/dek/Documents/Turtle/Tabellen/Tabellen.csv")
+
+
+with open(dateipfad2, mode="w", newline="", encoding="utf-8") as ausgabedatei:
+    writer = csv.writer(ausgabedatei, delimiter=";")
+    for name in überschriften:
+        writer.writerow([name])
+
+print(f"CSV-Tabelle '{dateipfad2}' wurde gespeichert.")
+
+
+
+
+
+
+
---
+++ Tabelle säubern.py
@@ -0,0 +1,34 @@
+import csv
+from pathlib import Path
+import numpy as np
+import pandas as pd
+
+
+pfad = Path("C:/Users/dek/Documents/Turtle/Tabellen/Tabellen.csv")
+
+df = pd.read_csv(pfad, sep=";", header=None, encoding="utf-8")
+überschriften = df.iloc[:, 0].tolist()
+
+
+Eingabeordner = Path("C:/Users/dek/Documents/Turtle/Tabellen")
+Ausgabeordner = Path("C:/Users/dek/Documents/Turtle/TabellenSauber")
+
+
+for i in range(len(überschriften)):
+    Eingabedatei = Eingabeordner / überschriften[i]
+    Ausgabedatei = Ausgabeordner / überschriften[i]
+
+    df = pd.read_csv(Eingabedatei, sep =";", encoding="utf-8")
+    df = df.drop(columns=[" Vehicle Type"])
+
+    df[" in gate"] = df[" in gate"].replace({"(L)": 0,"(R)": 1})
+    df[" out gate"] = df[" out gate"].replace({"(L)": 0,"(R)": 1})
+    df[" in time"] = pd.to_timedelta(df[" in time"].astype(str)).dt.total_seconds().astype(int)
+    df[" out time"] = pd.to_timedelta(df[" out time"].astype(str)).dt.total_seconds().astype(int)
+
+
+    df.to_csv(Ausgabedatei, sep=";", index=False, encoding="utf-8")
+    print(f"Tabelle {Ausgabedatei} wurde gespeichert")
+
+
+
